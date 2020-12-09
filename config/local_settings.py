2c473195ebf5458843c6f9d3ce1426513ae7b180
Ashiduka Kouhei <s1f101802252@iniad.org>
2020-12-09 19:18:04 +0900

    直らない

--- config/local_settings.py
+++ config/local_settings.py
@@ -1,23 +1,9 @@
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-
-# SECURITY WARNING: don't run with debug turned on in production!
-
-# Application definition
-print("Debug mode")
-
-SECRET_KEY = '!6rz8gx38ci27@(2pqu#wz8crc1$(0wc4+2is=u@n8do2^vn$$'
+SECRET_KEY = '!6rz8gx38ci27@(2pqu#wz8crc1$(0wc4+2is=u@n8do2^vn$$'  # 追加
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# settings.pyからそのままコピー
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -25,4 +11,4 @@ DATABASES = {
     }
 }
 
-DEBUG = True  # ローカルでDebugできるようになります
+DEBUG = True
