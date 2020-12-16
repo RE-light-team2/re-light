50925e27960fa14b6455cc28abdf2e5ca214278f
Yuri Maeda <s1f101802601@iniad.org>
2020-12-16 13:32:39 +0900

    ハンバーガー修正２

--- relight/static/relight/js/query.js
+++ relight/static/relight/js/query.js
@@ -1,6 +1,6 @@
 
 
-$('.hum').on('click',function(){
+$('.humberger').on('click',function(){
     if($('#slideL').hasClass('off')){
       $('#slideL').removeClass('off');
       $('#slideL').animate({'marginLeft':'0px'},500).addClass('on');
@@ -20,3 +20,22 @@ $('.hum2').on('click',function(){
     $('#slideL').animate({'marginLeft':'0px'},500);
   }
 });
+
+/*
+$(function() {
+
+    var dis = 250;
+
+    $("button") .click(function() {   
+
+        $("body") .children() 
+                         .animate ({
+                           "margin-left" : "+=" + dis + "px"
+                         }, 200); 
+
+        dis *= -1;
+
+    }) ;
+
+}) ;
+*/
